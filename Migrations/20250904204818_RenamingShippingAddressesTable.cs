66bc8f202cf92971f3a35e02ebb08c8fda28ae06
Mohamed Yassin Elsafty <myassinelsafty@gmail.com>
2025-09-05 01:10:11 +0300

    Renaming ShippingAddresses to Addresses

---
+++ Migrations/20250904204818_RenamingShippingAddressesTable.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stores.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamingShippingAddressesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_ShippingAddresses_BillingAddressId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_ShippingAddresses_ShippingAddressId",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ShippingAddresses",
+                table: "ShippingAddresses");
+
+            migrationBuilder.RenameTable(
+                name: "ShippingAddresses",
+                newName: "Addresses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Addresses_BillingAddressId",
+                table: "Orders",
+                column: "BillingAddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Addresses_ShippingAddressId",
+                table: "Orders",
+                column: "ShippingAddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Addresses_BillingAddressId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Addresses_ShippingAddressId",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses");
+
+            migrationBuilder.RenameTable(
+                name: "Addresses",
+                newName: "ShippingAddresses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ShippingAddresses",
+                table: "ShippingAddresses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_ShippingAddresses_BillingAddressId",
+                table: "Orders",
+                column: "BillingAddressId",
+                principalTable: "ShippingAddresses",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_ShippingAddresses_ShippingAddressId",
+                table: "Orders",
+                column: "ShippingAddressId",
+                principalTable: "ShippingAddresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
